6f6e82044f2ec6a2ea30836d2da3ce055dbe19e0
Dimitar Vukman <divukman@gmail.com>
2020-09-07 13:20:55 +0200

    Some algorithms example solutions.

--- AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/StringsEdits.java
+++ AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/StringsEdits.java
@@ -1,6 +1,8 @@
 package com.dimitar.algorithms.mix;
 
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * // Challenge: There are three types of edits that can be performed on strings:
@@ -38,34 +40,49 @@ public class StringsEdits {
 
         final HashMap<Character, Integer> hashMap1 = new HashMap<>();
         final HashMap<Character, Integer> hashMap2= new HashMap<>();
+        final HashMap<Character, Integer> hashMapDelta = new HashMap<>();
 
         buildHashMap(hashMap1, str1);
         buildHashMap(hashMap2, str2);
 
         // 2. Delta has to be exactly one or zero for one or zero edits away
         final String longerStr = str1.length() > str2.length() ? str1 : str2;
-        final String shorterStr = str1.length() < str2.length() ? str1 : str2;
+        final String shorterStr = str1.length() <= str2.length() ? str1 : str2;
 
-        int delta = 0;
-        Character deltaKey = null;
+
         for (int i = 0; i < longerStr.length(); i++) {
             final Character key = longerStr.charAt(i);
             final int counter1 = hashMap1.containsKey(key) ? hashMap1.get(key) : 0;
             final int counter2 = hashMap2.containsKey(key) ? hashMap2.get(key) : 0;
-            delta += Math.abs(counter1 - counter2);
-            if (delta == 1) deltaKey = key;
-            if (delta > 1) break;
+
+            if (!hashMapDelta.containsKey(key)) {
+                final int deltaForKey = Math.abs(counter1 - counter2);
+                hashMapDelta.put(key, deltaForKey);
+            }
         }
 
-        if (delta > 1) {
+        // Now our delta hash map has, for each character, differences between strings
+        AtomicInteger diff = new AtomicInteger();
+        hashMapDelta.keySet().forEach(key -> {
+            diff.addAndGet(hashMapDelta.get(key));
+        });
+
+        if (diff.intValue() > 1) {
             return false;
-        }
+        } else if (diff.intValue() == 1) {
+            //Find the diff char
+            AtomicReference<Character> diffChar = new AtomicReference<>();
+            hashMapDelta.keySet().forEach(key -> {
+               int val = hashMapDelta.get(key);
+               if (val == 1) {
+                   diffChar.set(key);
+               }
+            });
 
-        if (delta == 0 && !str1.equals(str2)) return false;
-
-        if (delta == 1) {
-            String replaced = longerStr.replace(deltaKey.toString(), "");
-            if (!replaced.equals(shorterStr)) return false;
+            final String newLongerStr = longerStr.replace(diffChar.toString(), "");
+            return newLongerStr.equals(shorterStr);
+        } else if (diff.intValue() == 0) { // palindrome?
+            return longerStr.equals(shorterStr);
         }
 
 
@@ -73,12 +90,12 @@ public class StringsEdits {
     }
 
     public static void main(String[] args) {
-        System.out.println(oneAway("pale", "paleXXXX"));
-        System.out.println(oneAway("pale", "ple"));
-        System.out.println(oneAway("pales", "pale"));
-        System.out.println(oneAway("Mate", "Mate"));
-        System.out.println(oneAway("miki", "kimi"));
-
-        System.out.println(oneAway("ABCD", "BACDG"));
+        System.out.println("oneAway(\"pale\", \"paleXXXX\"):   " + oneAway("pale", "paleXXXX"));
+        System.out.println("oneAway(\"pale\", \"ple\"):        " + oneAway("pale", "ple"));
+        System.out.println("oneAway(\"pales\", \"pale\"):      " + oneAway("pales", "pale"));
+        System.out.println("oneAway(\"Mate\", \"Mate\"):       " + oneAway("Mate", "Mate"));
+        System.out.println("oneAway(\"miki\", \"kimi\"):       " + oneAway("miki", "kimi"));
+        System.out.println("oneAway(\"ABCD\", \"BACDG\"):      " + oneAway("ABCD", "BACDG"));
+        System.out.println("oneAway(\"ABCD\", \"ABDC\"):       " + oneAway("ABCD", "ABDC"));
     }
 }
