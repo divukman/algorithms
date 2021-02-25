fdf6594af92e339973f2f35df46f8419305c7e81
dimitar <dimitar.vukman@bluedropmedical.com>
2021-02-25 16:33:25 +0100

    Algorithms

--- AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/Palindrome2.java
+++ AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/Palindrome2.java
@@ -26,13 +26,49 @@ public class Palindrome2 {
         return result;
     }
 
+
+
+    public static boolean isPalindrome2(final String str) {
+        boolean result = true;
+
+        int ptrFront = 0;
+        int ptrBack = str.length() - 1;
+
+        while (ptrFront < ptrBack) {
+            final Character charFront = str.charAt(ptrFront);
+            final Character charBack = str.charAt(ptrBack);
+            if (charBack.compareTo(charFront) != 0) {
+                result = false;
+                break;
+            } else {
+                ptrFront++;
+                ptrBack--;
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isPalindrome3(final String str) {
+        boolean result = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Enter a word: ");
         final String input = new Scanner(System.in).nextLine();
 
         System.out.println("You have entered: " + input);
         System.out.println("--- --- ---");
-        System.out.println("Is palindrome: " + Palindrome2.isPalindrome(input));
+        System.out.println("Is palindrome: " + Palindrome2.isPalindrome3(input));
     }
 
 }
