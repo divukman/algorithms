3b3f6603668b062e7d0cf3e8aaefcdb13d573b9a
dimitar <dimitar.vukman@bluedropmedical.com>
2021-03-03 14:09:16 +0100

    Algorithms

--- AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/Perm.java
+++ AlgorithmsTesting/src/main/java/com/dimitar/algorithms/mix/Perm.java
@@ -1,5 +1,6 @@
 package com.dimitar.algorithms.mix;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -48,6 +49,33 @@ public class Perm {
         return result;
     }
 
+
+    /**
+     * One trick solution:
+     * - create char array of string 1
+     * - create char array of string 2
+     * - sort arrays
+     * - compare arrays, if equal content then permutation is true
+     * @return true if string 1 is a permutation of string 2 and vice versa
+     */
+    public static boolean isPerm2(final String str1, final String str2) {
+        boolean result = true;
+
+        if (str1 == null || str2 == null || str1.length() != str2.length()) {
+            return false;
+        }
+
+        final char[] arr1 = str1.toCharArray();
+        final char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        result = Arrays.equals(arr1, arr2);
+
+        return result;
+    }
+
     public static void main(String[] args) {
         final String str1 = "thisis1";
         final String str2 = "1thisis";
@@ -55,10 +83,10 @@ public class Perm {
 
         System.out.println("Is Permutation: ");
         System.out.println("*** *** *** ***");
-        System.out.println("isPermutation ( " + str1 + ", " + str2 + " ) : " + Perm.isPerm(str1, str2));
-        System.out.println("isPermutation ( " + str1 + ", " + str3 + " ) : " + Perm.isPerm(str1, str3));
-        System.out.println("isPermutation ( " + str2 + ", " + str3 + " ) : " + Perm.isPerm(str2, str3));
-        System.out.println("isPermutation ( abc, cab ) : " + Perm.isPerm("abc", "cab"));
+        System.out.println("isPermutation ( " + str1 + ", " + str2 + " ) : " + Perm.isPerm2(str1, str2));
+        System.out.println("isPermutation ( " + str1 + ", " + str3 + " ) : " + Perm.isPerm2(str1, str3));
+        System.out.println("isPermutation ( " + str2 + ", " + str3 + " ) : " + Perm.isPerm2(str2, str3));
+        System.out.println("isPermutation ( abc, cab ) : " + Perm.isPerm2("abc", "cab"));
         System.out.println("*** *** *** ***");
     }
 }
